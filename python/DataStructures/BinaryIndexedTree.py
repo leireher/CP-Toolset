fdcff0c7ab57baface7feab197e914673531da66
Xabier Lahuerta Vázquez <xlahuerta@protonmail.com>
2022-04-20 19:37:00 +0200

    Fix method overloading

--- python/DataStructures/BinaryIndexedTree.py
+++ python/DataStructures/BinaryIndexedTree.py
@@ -2,21 +2,22 @@ def LSOne(s):
     return s & -s
 
 
+# Queries for dynamic RSQ in O(log n), elements numbered from 1 to n
 class FenwickTree:
     def __init__(self, n):
         self.ft = [0 for _ in range(n + 1)]
 
-    def rsq(self, b):
+    def get_sum(self, a):
         sum = 0
-        while b > 0:
-            sum += self.ft[b]
-            b -= LSOne(b)
+        while a > 0:
+            sum += self.ft[a]
+            a -= LSOne(a)
         return sum
 
-    def rsq(self, a, b):
-        return self.rsq(b) - (0 if a == 1 else self.rsq(a - 1))
+    def get_range_sum(self, a, b):
+        return self.get_sum(b) - (0 if a == 1 else self.get_sum(a - 1))
 
     def adjust(self, k, v):
-        while k < len(self.ft):
+        while k <= len(self.ft):
             self.ft[k] += v
             k += LSOne(k)
